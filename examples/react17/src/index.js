eeac42996e9eafaa493820388995ccf316072e97
yiludege <xzd123@qq.com>
2022-10-14 18:04:39 +0800

    v1.0.0-rc.23

--- examples/react17/src/index.js
+++ examples/react17/src/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import HelLibProxy from "hel-lib-proxy";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import * as shareComps from "./share-comps";
 import "./index.css";
+
+HelLibProxy.libReady('react17', shareComps);
 
 ReactDOM.render(
   // 严格模式，antd的弹窗会warning
-    <App />,
+  <App />,
   document.getElementById("root")
 );
 // If you want to start measuring performance in your app, pass a function
---
+++ examples/react17/src/share-comps/Test.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Test() {
+  return <div>
+    I am a comp from react17 project
+  </div>
+}
